47ec6a9edc4472f935a40f0417ea330790476b18
MihailCap <134375874+MihailCap@users.noreply.github.com>
2023-05-23 10:58:02 +0200

    wip

--- CounterApi/Counter.cs
+++ CounterApi/Counter.cs
@@ -2,9 +2,28 @@
 {
     public class Counter : ICounter
     {
+        public int Value { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Step { get; set; }
+        public void Increment()
+        {
+            Step > 0 ? Value += Step : Value++;
+        }
 
+        public void Decrement() { }
 
+        public void Reset()
+        {
 
+        }
+
+        public void Update(int Min, int Max, int Step)
+        {
+
+        }
+
+  
 
 
 
--- CounterApi/ICounter.cs
+++ CounterApi/ICounter.cs
@@ -2,5 +2,13 @@
 {
     public interface ICounter
     {
+        int Value { get; }
+        int Min { get; }
+        int Max { get; }
+        int Step { get; }
+        void Increment();
+        void Decrement();
+        void Reset();
+        void Update(int min, int max, int step);
     }
 }
