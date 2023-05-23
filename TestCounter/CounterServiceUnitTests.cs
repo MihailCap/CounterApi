d6ab6e16887200d4798f039383453020fb9ae7af
Luca Fabbri <luca@zepfiro.com>
2023-05-23 15:10:23 +0200

    Update CounterServiceUnitTests.cs

--- TestCounter/CounterServiceUnitTests.cs
+++ TestCounter/CounterServiceUnitTests.cs
@@ -35,6 +35,21 @@ public class CounterServiceUnitTests
     }
 
     /// <summary>
+    /// Test get one
+    /// </summary>
+    [Test]
+    public void TestGet()
+    {
+        ICounterService service = new CounterService(context);
+        Assert.Multiple(() =>
+        {
+            Assert.That(service.Get("test1"), Is.Not.Null);
+            Assert.That(service.Get("test2"), Is.Not.Null);
+            Assert.That(service.Get("test3"), Is.Not.Null);
+        });
+    }
+
+    /// <summary>
     /// Test create counter
     /// </summary>
     [Test]
@@ -58,7 +73,7 @@ public class CounterServiceUnitTests
     {
         ICounterService service = new CounterService(context);
 
-        Assert.Throws<Exception>(()=>service.Create("test1"));
+        Assert.That(service.Create("test1"),Is.Null);
     }
 
     [Test]
@@ -70,11 +85,18 @@ public class CounterServiceUnitTests
         
         Assert.Multiple(() =>
         {
-            Assert.That(counter.Name, Is.EqualTo("test1"));
-            Assert.That(counter.Min, Is.Null);
-            Assert.That(counter.Max, Is.Null);
-            Assert.That(counter.Step, Is.EqualTo(1));
-            Assert.That(counter.Value, Is.EqualTo(0));
+            Assert.That(counter, Is.Not.Null);
+        });
+        
+        counter = service.Get("test1");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter?.Name, Is.EqualTo("test1"));
+            Assert.That(counter?.Min, Is.Null);
+            Assert.That(counter?.Max, Is.Null);
+            Assert.That(counter?.Step, Is.EqualTo(1));
+            Assert.That(counter?.Value, Is.EqualTo(0));
             Assert.That(service.GetAll().Count(), Is.EqualTo(3));
         });
     }
@@ -88,7 +110,128 @@ public class CounterServiceUnitTests
         
         Assert.Multiple(() =>
         {
-            Assert.That(service.GetAll().Where(x => x.Name == "updated").Count(), Is.EqualTo(1));
+            Assert.That(counter, Is.Not.Null);
+            Assert.That(service.GetAll().Count(x => x.Name == "updated"), Is.EqualTo(1));
+        });
+    }
+
+    [Test]
+    public void TestUpdateStep()
+    {
+        ICounterService service = new CounterService(context);
+
+        var counter = service.Update("test1", null, null, 2);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter, Is.Not.Null);
+        });
+        
+        counter = service.Get("test1");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter?.Step, Is.EqualTo(2));
+        });
+    }
+
+    [Test]
+    public void TestUpdateStepNull()
+    {
+        ICounterService service = new CounterService(context);
+
+        var counter = service.Update("test1", null, null, 2);
+        counter = service.Update(counter?.Name, null, null, null);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter, Is.Not.Null);
+        });
+        
+        counter = service.Get("test1");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter?.Step, Is.EqualTo(2));
+        });
+    }
+
+    [Test]
+    public void TestUpdateStepFail()
+    {
+        ICounterService service = new CounterService(context);
+
+        var counter = service.Update("test1", null, null, -2);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter, Is.Null);
+        });
+    }
+
+    [Test]
+    public void TestUpdateMinMax()
+    {
+        ICounterService service = new CounterService(context);
+
+        var counter = service.Update("test1", 2, 1, null);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter, Is.Not.Null);
+        });
+        
+        counter = service.Get("test1");
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter?.Max, Is.EqualTo(2));
+            Assert.That(counter?.Min, Is.EqualTo(1));
+        });
+    }
+
+    [Test]
+    public void TestUpdateMinMaxFail()
+    {
+        ICounterService service = new CounterService(context);
+
+        var counter = service.Update("test1", 1, 2, null);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(counter, Is.Null);
+        });
+    }
+
+    [Test]
+    public void TestIncrement()
+    {
+        ICounterService service = new CounterService(context);
+        Assert.That(service.Increment("test2")?.Value, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TestDecrement()
+    {
+        ICounterService service = new CounterService(context);
+        Assert.That(service.Decrement("test3")?.Value, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void TestReset()
+    {
+        ICounterService service = new CounterService(context);
+        Assert.That(service.Reset("test3")?.Value, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void TestDelete()
+    {
+        ICounterService service = new CounterService(context);
+        Assert.Multiple(() =>
+        {
+            Assert.That(service.Delete("test3"), Is.True);
+            Assert.That(service.GetAll().Count(), Is.EqualTo(2));
         });
     }
 }
