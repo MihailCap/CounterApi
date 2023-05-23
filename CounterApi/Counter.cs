ef14e9dedb4bf65bd2ad86c80fc97871c3996ee9
MihailCap <134375874+MihailCap@users.noreply.github.com>
2023-05-23 10:44:16 +0200

    Inizio

---
+++ CounterApi/Counter.cs
@@ -0,0 +1,20 @@
+﻿public class Counter
+{
+    public int Valore { get; set; }
+    public int Max { get; set; }
+    public int Min { get; set; }
+    public int Incremento { get; set; }
+
+    public void Incrementa(int Incremento)
+    {
+
+    }
+
+
+
+
+
+
+
+
+}
