821ae2bb6f9dfbb410629843fe0ee21c9a41d073
MihailCap <134375874+MihailCap@users.noreply.github.com>
2023-05-24 10:24:41 +0200

    Update Program.cs

--- CounterApi/Program.cs
+++ CounterApi/Program.cs
@@ -1,8 +1,10 @@
 using CounterApi.Persistence;
+using CounterApi.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<CounterDb>(opt => opt.UseInMemoryDatabase("CounterList"));
+builder.Services.AddTransient<ICounterService, CounterService>();
 var app = builder.Build();
 
 
