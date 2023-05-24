7e888c8e492e735fdea37c21e86f07bde130b47f
MihailCap <134375874+MihailCap@users.noreply.github.com>
2023-05-24 10:44:10 +0200

    wip

--- CounterApi/Program.cs
+++ CounterApi/Program.cs
@@ -11,16 +11,16 @@ var app = builder.Build();
 // Get all
 app.MapGet("/counter", (ICounterService service) => service.GetAll());
 //Get one by name
-app.MapGet("/counter/{name}", () => { });
+app.MapGet("/counter/{name}", (string name, ICounterService service) => service.Get(name));
 //Create counter
-app.MapPost("/counter", () => { });
+app.MapPost("/counter", (string name, ICounterService service) => service.Create(name));
 //Update counter
-app.MapPut("/counter/{name}", () => { });
+app.MapPut("/counter/{name}", (string name, ) => { });
 //Increment counter
-app.MapPatch("/counter/{name}/increment", () => { });
+app.MapPatch("/counter/{name}/increment", (string name, ICounterService service) => service.Increment(name));
 //Decrement counter
-app.MapPatch("/counter/{name}/decrement", () => { });
+app.MapPatch("/counter/{name}/decrement", (string name, ICounterService service) => service.Decrement(name));
 //Reset counter
-app.MapPatch("/counter/{name}/reset", () => { });
+app.MapPatch("/counter/{name}/reset", (string name, ICounterService service) => service.Reset(name) );
 
 app.Run();
