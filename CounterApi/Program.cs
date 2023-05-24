87d0bdf078803526fb252e4cde258cbdde822717
Luca Fabbri <luca@zepfiro.com>
2023-05-24 10:31:15 +0200

    Update Program.cs

--- CounterApi/Program.cs
+++ CounterApi/Program.cs
@@ -1,5 +1,6 @@
 using CounterApi.Persistence;
 using CounterApi.Service;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,9 +8,19 @@ builder.Services.AddDbContext<CounterDb>(opt => opt.UseInMemoryDatabase("Counter
 builder.Services.AddTransient<ICounterService, CounterService>();
 var app = builder.Build();
 
-
-
-
-
+// Get all
+app.MapGet("/counter", (ICounterService service) => service.GetAll());
+//Get one by name
+app.MapGet("/counter/{name}", () => { });
+//Create counter
+app.MapPost("/counter", () => { });
+//Update counter
+app.MapPut("/counter/{name}", () => { });
+//Increment counter
+app.MapPatch("/counter/{name}/increment", () => { });
+//Decrement counter
+app.MapPatch("/counter/{name}/decrement", () => { });
+//Reset counter
+app.MapPatch("/counter/{name}/reset", () => { });
 
 app.Run();
