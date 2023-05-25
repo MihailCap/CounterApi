d27fb310b5676185bcf4793c9255f8a1d6ee2a52
MihailCap <134375874+MihailCap@users.noreply.github.com>
2023-05-25 14:31:45 +0200

    wip

---
+++ contatore-vue/src/CounterIndex.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import axios from 'axios';
+    import { ref } from 'vue';
+    let name = ref<string|null>(null);
+    
+    
+
+  const creaCounters = () => {
+    axios.post('http://localhost:5053/', { input: name })
+      .then(response => {
+        
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+
+</script>
+
+<template>
+    <label for="name">nome</label>
+    <input type="text" v-model="name" />
+    <button @click="creaCounters()">crea </button>
+
+</template>
+
+<style>
+</style>
